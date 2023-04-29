751266d96a3415c88c62808f88967ab023068413
bensboto <bensboto@gmail.com>
2023-04-29 17:20:37 -0600

    added reactions, still need to fix delete

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require("../models");
+const { User, Thought, Reaction } = require("../models");
 
 module.exports = {
   //get all available thoughts
@@ -58,6 +58,44 @@ module.exports = {
   async deleteThought({ params }, res) {
     try {
       const dbThoughtData = await Thought.findOneAndDelete({ _id: params.id });
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this id!" });
+        return;
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //friends
+  //Post - create reaction
+  async createReaction({ params, body }, res) {
+    try {
+      const newReaction =  await Reaction.create(body); // create new Reaction object with data from request body
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $addToSet: { reactions: newReaction } }, // add the new Reaction object to the reactions array
+        { new: true, runValidators: true }
+      );
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this id!" });
+        return;
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //Delete - remove reaction
+  async deleteReaction({ params,body}, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: body.reactionId } } },
+        { new: true }
+      );
       if (!dbThoughtData) {
         res.status(404).json({ message: "No thought found with this id!" });
         return;
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -5,11 +5,22 @@ const {
   getThoughtById,
   createThought,
   updateThought,
-  deleteThought
+  deleteThought,
+  createReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtsController");
 
 router.route("/").get(getAllThoughts).post(createThought);
 
-router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router
+  .route("/:thoughtId/reactions")
+  .post(createReaction)
+  .delete(deleteReaction);
 
 module.exports = router;
